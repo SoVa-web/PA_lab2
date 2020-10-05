57c5cf49ceb3187af4e1b435031a5e9a8e612724
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-10-06 02:33:41 +0300

    classes for ants and colon

--- Suprun_IP96_lab2.cpp
+++ Suprun_IP96_lab2.cpp
@@ -2,6 +2,7 @@
 #include<ctime>
 #include<random>
 #include<iomanip>
+#include<vector>
 
 using namespace std;
 
@@ -10,12 +11,36 @@ class Graph{
  //functions
    Graph(int number);
    void initMatrix();
+   void initPheramons();
    void generDistances();
    void output();
 
 //variaties 
 int numberNodes;
 int** distanceMatrix;
+double** pheramonsMatrix;
+};
+
+
+class Ant{
+public:
+//functions
+ Ant();
+
+//variaties
+vector<int>nodesTaboo;
+};
+
+class ColonAnts{
+public:
+ double alpha = 3;//
+ double beta = 2;//
+ double evaporate = 0.7;//коефіцієнт випаровування
+ double tauStart = 1;//початковий рівень феромону
+ int eliteAnts = 10;
+ int traditionalAnts = 35;
+double pheromonsByOne = 5;
+double pheromonsByElite = 10;
 };
 
 Graph::Graph(int number){
@@ -33,10 +58,18 @@ void Graph::initMatrix(){
     }
 }
 
+void Graph::initPheramons(){
+    srand(time(NULL));
+    pheramonsMatrix = new double*[numberNodes];
+    for(int i =0; i < numberNodes; i++){
+        pheramonsMatrix[i] = new double[numberNodes];
+    }
+}
+
 void Graph::generDistances(){
     for(int i =0; i < numberNodes; i++){
         for(int j =0; j < numberNodes; j++){
-            distanceMatrix[i][j] = rand()%100;
+            distanceMatrix[i][j] = rand()%40;
             if(i==j)
             distanceMatrix[i][j]=0;
         }
