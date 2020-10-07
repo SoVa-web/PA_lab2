47c8b0b464209fa71f9a5f6da1bd865d517fa16d
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-10-08 01:22:28 +0300

    algorithm

--- Suprun_IP96_lab2.cpp
+++ Suprun_IP96_lab2.cpp
@@ -3,27 +3,11 @@
 #include<random>
 #include<iomanip>
 #include<vector>
+#include<cmath>
 
 using namespace std;
 
-class Graph{
-   
- public:
- //functions
-   Graph(){};
-   Graph(int number);
-   void initMatrix();
-   void initPheramons();
-   void generDistances();
-   void initVision();
-   void output();
 
-//variaties 
-int numberNodes;
-int** distanceMatrix;
-double** pheramonsMatrix;
-double** visionMaxtrix;
-};
 
 class Ant{
 public:
@@ -31,8 +15,36 @@ public:
  Ant(){};
 
 //variaties
-vector<int>nodesTaboo;
- bool areYouElyte;
+vector<int>way;
+int optL =0;;
+bool areYouElyte;
+double deltaTau =0;
+};
+
+class Algorithm{
+ public:
+ Algorithm(Ant ant, int number);
+   void initMatrix();
+   void initPheramons();
+   void generDistances();
+   void initVision();
+   void output();
+   void greedySearch();
+   void placingAnts();
+   void algorithm();
+   int probability(int i, vector<int> j);
+   double tau(int minL, int optL, bool elit);
+
+   
+   //variaties 
+int numberNodes;
+int** distanceMatrix;
+double** pheramonsMatrix;
+double** visionMaxtrix;
+double minL;
+ int numberAnts;
+ Ant* ants;
+ int* placeAnts;
  double alpha = 3;//
  double beta = 2;//
  double evaporate = 0.7;//коефіцієнт випаровування
@@ -41,91 +53,94 @@ vector<int>nodesTaboo;
  int traditionalAnts = 35;
 double pheromonsByOne = 5;
 double pheromonsByElite = 10;
+const int iter = 10;
 };
 
-
-class AntAlgorithm{
-public:
-//func
-AntAlgorithm(Graph graph,  int number);
-AntAlgorithm(Ant ant);
-void greedySearch();
-//var
-double optimL;
-double minL;
-Graph graph;
- int numberAnts;
- Ant* ants;
-};
-
-
-AntAlgorithm::AntAlgorithm(Graph graph,  int number){
-this->graph = graph;
-numberAnts = number;
-greedySearch();
-}
-
-AntAlgorithm::AntAlgorithm(Ant ant){
-ants = new Ant[numberAnts];
- for(int i =1; i < numberAnts+1; i++){
-   if(numberAnts-ant.eliteAnts)
-     ants[i-1].areYouElyte  = false;
-    else
-     ants[i-1].areYouElyte  = true;
-  }
-}
-
-void AntAlgorithm::greedySearch(){
-    this->minL = 0;
-    vector<int> way;
-    vector<int> nodes;
-    bool visited[graph.numberNodes];
-    for(int i =0; i<graph.numberNodes; i++){
-        visited[i] = true;
-    }
-    int next = 0;
-    visited[next] = false;
-    way.push_back(next);
-    while (way.size()<graph.numberNodes){
-        for(int i =0; i < graph.numberNodes; i++){
-            if(next != i && visited[i] ){
-              nodes.push_back(i);
-            }
-        }
-        int dist = graph.distanceMatrix[next][nodes[0]];
-        int k =next;
-       for(int i =0; i < nodes.size(); i++){
-          if(dist >= graph.distanceMatrix[next][nodes[i]]){
-              dist = graph.distanceMatrix[next][nodes[i]];
-              k = nodes[i];
-          }
-       }
-       next =k;
-        visited[next] = false;
-        way.push_back(next);
-        minL+=graph.distanceMatrix[way.size()-2][way.size()-1];
-        nodes.clear();
-    }
-    minL+=graph.distanceMatrix[way.size()-1][0];
-    way.push_back(way[0]);
-    cout<<"Lmin"<<minL<<endl;
-    cout<<"Way: ";
-    for(int i =0 ;i < way.size(); i++){
-        cout<<way[i]<<"-->";
-    }
-    way.clear();
-}
-
-Graph::Graph(int number){
-  numberNodes = number;
+Algorithm::Algorithm(Ant ant, int number){
+    numberNodes = number;
   initMatrix();
   generDistances();
   initPheramons();
   initVision();
   output();
+  numberAnts = eliteAnts+traditionalAnts;
+  greedySearch();
+  ants = new Ant[numberAnts];
+ for(int i =0; i < numberAnts; i++){
+   if(numberAnts-eliteAnts)
+     ants[i].areYouElyte  = false;
+    else
+     ants[i].areYouElyte  = true;
+  }
 }
 
-void Graph::initMatrix(){
+void Algorithm::algorithm(){
+   for(int i =0; i< iter; i++){//time life
+      for(int j =0; j < numberAnts; j++){//cycle for ants
+           vector<int> nodes;
+           bool visited[numberNodes];
+           for(int j =0; j < numberNodes; j++){
+              visited[i] = true;
+           }
+            int next = 0;
+            visited[next] = false;
+            ants[j].way.push_back(next);
+    while (ants[j].way.size()<numberNodes){
+        for(int k =0; k < numberNodes; k++){
+            if(next != k && visited[k] ){
+              nodes.push_back(k);
+            }
+        }
+        int prob = probability(next, nodes);//next node
+        next = prob;
+        visited[next] = false;
+        ants[j].way.push_back(next);
+        ants[j].optL+=distanceMatrix[ants[j].way.size()-2][ants[j].way.size()-1];
+        nodes.clear();
+    }
+    ants[j].optL+=distanceMatrix[ants[j].way.size()-1][0];
+    ants[j].way.push_back(ants[j].way[0]);
+      ants[j].deltaTau = tau(minL, ants[j].optL, ants[j].areYouElyte);
+      }//end cycle of  ants
+      //оновлення параметрів
+   }
+}
+
+double Algorithm::tau(int minL, int optL, bool elit ){
+    double delta =0;
+
+    return delta;
+}
+
+int Algorithm::probability(int i, vector<int> j){
+ double sum = 0;
+ for(int k =0; k < j.size(); k++){
+    sum+=pow(pheramonsMatrix[i][j[k]], alpha )*pow(visionMaxtrix[i][j[k]], beta);
+ }
+ vector<double> prob;
+ for(int k =0; k < j.size(); k++){
+     int a = (pow(pheramonsMatrix[i][j[k]], alpha )*pow(visionMaxtrix[i][j[k]], beta))/sum;
+ }
+ double b = prob[0];
+ int maximum = 0; 
+ for(int k =0; k < prob.size(); k++){
+     if(b<=prob[k]){
+         b = prob[k];
+         maximum = k;
+     }
+ }
+return j[maximum];
+}
+
+void Algorithm::placingAnts(){
+    placeAnts = new int[eliteAnts+traditionalAnts];
+    for(int i =0; i < numberAnts; i++){
+        placeAnts[i] = rand()%numberNodes;
+        cout<<placeAnts[i]<<" ";
+    }
+}
+
+void Algorithm::initMatrix(){
     srand(time(NULL));
     distanceMatrix = new int*[numberNodes];
     for(int i =0; i < numberNodes; i++){
@@ -133,7 +148,7 @@ void Graph::initMatrix(){
     }
 }
 
-void Graph::initPheramons(){
+void Algorithm::initPheramons(){
     srand(time(NULL));
     pheramonsMatrix = new double*[numberNodes];
     for(int i =0; i < numberNodes; i++){
@@ -150,7 +165,7 @@ void Graph::initPheramons(){
     }
 }
 
-void Graph::initVision(){
+void Algorithm::initVision(){
     srand(time(NULL));
     visionMaxtrix = new double*[numberNodes];
     for(int i =0; i < numberNodes; i++){
@@ -173,7 +188,7 @@ void Graph::initVision(){
     }
 }
 
-void Graph::generDistances(){
+void Algorithm::generDistances(){
     for(int i =0; i < numberNodes; i++){
         for(int j =0; j < numberNodes; j++){
             distanceMatrix[i][j] = 1 + rand()%40;
@@ -184,7 +199,7 @@ void Graph::generDistances(){
     }
 }
 
-void Graph::output(){
+void Algorithm::output(){
     for(int i =0; i < numberNodes; i++){
         for(int j =0; j < numberNodes; j++){
             cout<<setw(5)<<distanceMatrix[i][j]<<" ";
@@ -193,10 +208,50 @@ void Graph::output(){
     }
 }
 
+void Algorithm::greedySearch(){
+    this->minL = 0;
+    vector<int> way;
+    vector<int> nodes;
+    bool visited[numberNodes];
+    for(int i =0; i<numberNodes; i++){
+        visited[i] = true;
+    }
+    int next = 0;
+    visited[next] = false;
+    way.push_back(next);
+    while (way.size()<numberNodes){
+        for(int i =0; i < numberNodes; i++){
+            if(next != i && visited[i] ){
+              nodes.push_back(i);
+            }
+        }
+        int dist = distanceMatrix[next][nodes[0]];
+        int k =next;
+       for(int i =0; i < nodes.size(); i++){
+          if(dist >= distanceMatrix[next][nodes[i]]){
+              dist = distanceMatrix[next][nodes[i]];
+              k = nodes[i];
+          }
+       }
+       next =k;
+        visited[next] = false;
+        way.push_back(next);
+        minL+=distanceMatrix[way.size()-2][way.size()-1];
+        nodes.clear();
+    }
+    minL+=distanceMatrix[way.size()-1][0];
+    way.push_back(way[0]);
+    cout<<"Lmin"<<minL<<endl;
+    cout<<"Way: ";
+    for(int i =0 ;i < way.size(); i++){
+        cout<<way[i]<<"-->";
+    }
+    way.clear();
+}
+
+
 int main(){
-  Graph graph(7);
   Ant ant;
-  int numberAnts = ant.traditionalAnts+ant.eliteAnts;
-  AntAlgorithm algorithm(graph, numberAnts);
-  AntAlgorithm algo(ant);
+  Algorithm algo(ant, 46);
+  algo.placingAnts();
 }
